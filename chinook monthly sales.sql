f50bcc119a74c931c266a7b9ecba2aa6cca5e25d
M Asyhar <50800273+asyhar22@users.noreply.github.com>
2022-11-09 22:00:36 +0700

    Add files via upload

---
+++ chinook monthly sales.sql
@@ -0,0 +1,11 @@
+/* Tim sales ingin mengetahui bagaimana performa harian dan bulanan penjualan track kita pada seluruh gerai */
+
+WITH DF AS (
+	SELECT INVI.InvoiceLineId, INV.InvoiceDate, TR.name as TrackName, TR.Composer, GR.name as Genre, INVI.UnitPrice, INVI.Quantity, INVI.UnitPrice*INVI.Quantity as Sales
+	FROM invoice_items INVI
+	JOIN invoices INV on INVI.InvoiceId = INV.InvoiceId
+	JOIN tracks TR on INVI.TrackId = TR.TrackId 
+	JOIN genres GR on TR.GenreId = GR.GenreId 
+)
+
+SELECT strftime('%m', InvoiceDate) as Month, sum(Sales) as SubTotalSales FROM DF GROUP BY Month ORDER BY Month ASC
